5a51cef5ec888107c687a366bb82f4360754f068
Gauri-866 <sgauri866@gmail.com>
2026-07-03 12:04:09 +0530

    added the status response in get plant list

--- src/server/db/generated/prisma/enums.ts
+++ src/server/db/generated/prisma/enums.ts
@@ -37,7 +37,7 @@ export type UserStatus = (typeof UserStatus)[keyof typeof UserStatus]
 
 export const PlantStatus = {
   Offline: 'Offline',
-  Normal: 'Normal',
+  Online: 'Online',
   Abnormal: 'Abnormal',
   Standby: 'Standby'
 } as const
